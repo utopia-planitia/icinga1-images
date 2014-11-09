642acb1d69e52aad070472c9233fb1bbdb3cc5fa
Joachim Breitsprecher <jeyk@jeyk.de>
2014-11-09 02:02:29 +0100

    Fix permissions for command interface

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,6 @@ gpasswd -a www-data nagios
 mkdir -p /var/run/icinga/cmd
 chown -R nagios:nagios /var/run/icinga
 chown -R nagios:www-data /var/run/icinga/cmd
-chmod g+rwx /var/run/icinga/cmd
+chown -R nagios:www-data /var/lib/icinga/rw
+chmod g+rwx /var/lib/icinga/rw
 exec "$@"
