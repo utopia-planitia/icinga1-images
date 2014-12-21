3d16273e724088860e941b571cc73e8e8870a3ab
Joachim Breitsprecher <github@jeyk.de>
2014-12-21 15:03:02 +0100

    Set g+s bit on /var/lib/icinga/rw

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,5 @@ mkdir -p /var/run/icinga/cmd
 chown -R nagios:nagios /var/run/icinga
 chown -R nagios:www-data /var/run/icinga/cmd
 chown -R nagios:www-data /var/lib/icinga/rw
-chmod g+rwx /var/lib/icinga/rw
+chmod g+rws /var/lib/icinga/rw
 exec "$@"
