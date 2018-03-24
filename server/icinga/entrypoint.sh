f7bb2a3d597aa042e2345a33cbdaef846b2392eb
David Sauer <davedamoon@gmail.com>
2018-03-24 18:52:16 +0100

    check port 80, 443, 6443

--- server/icinga/entrypoint.sh
+++ server/icinga/entrypoint.sh
@@ -6,4 +6,9 @@ rm -rf /var/lib/icinga/rw
 mkdir -p /var/lib/icinga/rw
 chown -R nagios:www-data /var/lib/icinga/rw
 chmod g+rwxs /var/lib/icinga/rw
+
+if [ "$@" == "" ]; then
+    /usr/sbin/icinga /etc/icinga/icinga.cfg -v
+fi
+
 exec "$@"
