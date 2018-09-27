6f72b65b4cb4e1adb767feb66747744f681ae832
David Sauer <david.sauer@turbinekreuzberg.com>
2018-09-27 18:00:36 +0200

    changed permissions to allow a history to be written

--- server/icinga/entrypoint.sh
+++ server/icinga/entrypoint.sh
@@ -3,9 +3,13 @@ set -e
 
 gpasswd -a www-data nagios
 rm -rf /var/lib/icinga/rw
-mkdir -p /var/lib/icinga/rw
+mkdir -p                 /var/lib/icinga/rw
 chown -R nagios:www-data /var/lib/icinga/rw
-chmod g+rwxs /var/lib/icinga/rw
+chmod -R g+rwxs          /var/lib/icinga/rw
+touch                    /var/log/icinga/icinga.log
+chown -R nagios:www-data /var/log/icinga
+chmod -R g+rwxs          /var/log/icinga
+chmod -R o+rwx           /var/log/icinga
 
 if [ "$@" == "" ]; then
     /usr/sbin/icinga /etc/icinga/icinga.cfg -v
